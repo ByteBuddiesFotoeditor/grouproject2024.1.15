5b0a03986ffe687df4ba4d7c351c520332755bab
DexterWithouP <119928716+ko-kusang@users.noreply.github.com>
2024-01-15 02:27:15 +0800

    final

---
+++ fotoeditor/src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            fotoEditorGUI editor = new fotoEditorGUI();
+            editor.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            editor.setVisible(true);
+           
+        });
+    }
+}
